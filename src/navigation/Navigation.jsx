1dde43841c1c0e378b3fbd041f793d330526f8a2
DESKTOP-86563HB <samad123450099@gmail.com>
2024-06-29 19:44:18 +0500

    notfound page

--- src/navigation/Navigation.jsx
+++ src/navigation/Navigation.jsx
@@ -3,6 +3,7 @@ import Home from '../components/Home'
 import Details from '../components/Details'
 import Marketing from '../components/Markiting'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Notfontpage from '../pages/Notfontpage'
 
 function Navigation() {
   return (
@@ -12,6 +13,8 @@ function Navigation() {
                 <Route path='/' element={<Marketing />} />
                 <Route path='/details' element={<Details />} />
                 <Route path='/marketing' element={<Home/>} />
+                <Route path='*' element={<Notfontpage />} />
+
             </Routes>
         </BrowserRouter>
     </div>
