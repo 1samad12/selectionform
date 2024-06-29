c424c268eaeb90d5724c3be3d6d1688b9153a608
DESKTOP-86563HB <samad123450099@gmail.com>
2024-06-29 20:00:47 +0500

    update

--- src/pages/Notfontpage.jsx
+++ src/pages/Notfontpage.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Button, Result } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 function Notfontpage() {
+    const navigate = useNavigate();
+
   return (
     <div>
   <Result
-    status="403"
-    title="403"
+    status="404"
+    title="404"
     subTitle="Sorry, you are not authorized to access this page."
-    extra={<Button type="primary">Back Home</Button>}
+    extra={<Button type="primary" onClick={() => navigate('/')}>Back Home</Button>}
   />
     </div>
   )
